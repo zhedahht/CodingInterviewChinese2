ebd6955165613b0ae7320cb24bcc59e12c940cc6
Harry He <zhedahht@hotmail.com>
2016-11-20 21:09:45 -0800

    Initial commit for Interview question 1

---
+++ 01_AssignmentOperator/AssignmentOperator.cpp
@@ -0,0 +1,141 @@
+// AssignmentOperator.cpp : Defines the entry point for the console application.
+//
+
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 著作权所有者：何海涛
+
+// 面试题1：赋值运算符函数
+// 题目：如下为类型CMyString的声明，请为该类型添加赋值运算符函数。
+
+#include<cstring>
+#include<cstdio>
+
+class CMyString
+{
+public:
+    CMyString(char* pData = nullptr);
+    CMyString(const CMyString& str);
+    ~CMyString(void);
+
+    CMyString& operator = (const CMyString& str);
+
+    void Print();
+      
+private:
+    char* m_pData;
+};
+
+CMyString::CMyString(char *pData)
+{
+    if(pData == nullptr)
+    {
+        m_pData = new char[1];
+        m_pData[0] = '\0';
+    }
+    else
+    {
+        int length = strlen(pData);
+        m_pData = new char[length + 1];
+        strcpy(m_pData, pData);
+    }
+}
+
+CMyString::CMyString(const CMyString &str)
+{
+    int length = strlen(str.m_pData);
+    m_pData = new char[length + 1];
+    strcpy(m_pData, str.m_pData);
+}
+
+CMyString::~CMyString()
+{
+    delete[] m_pData;
+}
+
+CMyString& CMyString::operator = (const CMyString& str)
+{
+    if(this == &str)
+        return *this;
+
+    delete []m_pData;
+    m_pData = nullptr;
+
+    m_pData = new char[strlen(str.m_pData) + 1];
+    strcpy(m_pData, str.m_pData);
+
+    return *this;
+}
+
+// ====================测试代码====================
+void CMyString::Print()
+{
+    printf("%s", m_pData);
+}
+
+void Test1()
+{
+    printf("Test1 begins:\n");
+
+    char* text = "Hello world";
+
+    CMyString str1(text);
+    CMyString str2;
+    str2 = str1;
+
+    printf("The expected result is: %s.\n", text);
+
+    printf("The actual result is: ");
+    str2.Print();
+    printf(".\n");
+}
+
+// 赋值给自己
+void Test2()
+{
+    printf("Test2 begins:\n");
+
+    char* text = "Hello world";
+
+    CMyString str1(text);
+    str1 = str1;
+
+    printf("The expected result is: %s.\n", text);
+
+    printf("The actual result is: ");
+    str1.Print();
+    printf(".\n");
+}
+
+// 连续赋值
+void Test3()
+{
+    printf("Test3 begins:\n");
+
+    char* text = "Hello world";
+
+    CMyString str1(text);
+    CMyString str2, str3;
+    str3 = str2 = str1;
+
+    printf("The expected result is: %s.\n", text);
+
+    printf("The actual result is: ");
+    str2.Print();
+    printf(".\n");
+
+    printf("The expected result is: %s.\n", text);
+
+    printf("The actual result is: ");
+    str3.Print();
+    printf(".\n");
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+
+    return 0;
+}
+
