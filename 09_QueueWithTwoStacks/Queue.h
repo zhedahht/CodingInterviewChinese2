28406c195ac304439ca45484518dba4158a32443
Harry He <zhedahht@hotmail.com>
2016-11-22 23:03:10 -0800

    Initial commit for question 9

---
+++ 09_QueueWithTwoStacks/Queue.h
@@ -0,0 +1,65 @@
+//==================================================================
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 著作权所有者：何海涛
+//==================================================================
+
+// 面试题9：用两个栈实现队列
+// 题目：用两个栈实现一个队列。队列的声明如下，请实现它的两个函数appendTail
+// 和deleteHead，分别完成在队列尾部插入结点和在队列头部删除结点的功能。
+
+#pragma once
+#include <stack>
+#include <exception>
+
+using namespace std;
+
+template <typename T> class CQueue
+{
+public:
+    CQueue(void);
+    ~CQueue(void);
+    
+    // 在队列末尾添加一个结点
+    void appendTail(const T& node);
+
+    // 删除队列的头结点
+    T deleteHead();
+
+private:
+    stack<T> stack1;
+    stack<T> stack2;
+};
+
+template <typename T> CQueue<T>::CQueue(void)
+{
+}
+
+template <typename T> CQueue<T>::~CQueue(void)
+{
+}
+
+template<typename T> void CQueue<T>::appendTail(const T& element)
+{
+    stack1.push(element);
+} 
+
+template<typename T> T CQueue<T>::deleteHead()
+{
+    if(stack2.size()<= 0)
+    {
+        while(stack1.size()>0)
+        {
+            T& data = stack1.top();
+            stack1.pop();
+            stack2.push(data);
+        }
+    }
+
+    if(stack2.size() == 0)
+        throw new exception("queue is empty");
+
+    T head = stack2.top();
+    stack2.pop();
+
+    return head;
+}
---
+++ 09_QueueWithTwoStacks/QueueWithTwoStacks.cpp
@@ -0,0 +1,42 @@
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 著作权所有者：何海涛
+
+#include "Queue.h"
+
+// ====================测试代码====================
+void Test(char actual, char expected)
+{
+    if(actual == expected)
+        printf("Test passed.\n");
+    else
+        printf("Test failed.\n");
+}
+
+int main(int argc, char* argv[])
+{
+    CQueue<char> queue;
+
+    queue.appendTail('a');
+    queue.appendTail('b');
+    queue.appendTail('c');
+
+    char head = queue.deleteHead();
+    Test(head, 'a');
+
+    head = queue.deleteHead();
+    Test(head, 'b');
+
+    queue.appendTail('d');
+    head = queue.deleteHead();
+    Test(head, 'c');
+
+    queue.appendTail('e');
+    head = queue.deleteHead();
+    Test(head, 'd');
+
+    head = queue.deleteHead();
+    Test(head, 'e');
+
+    return 0;
+}
+
