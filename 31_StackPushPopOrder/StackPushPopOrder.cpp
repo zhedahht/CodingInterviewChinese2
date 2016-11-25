54c739ed54e05884acad2b0672522003b6d49f56
Harry He <zhedahht@hotmail.com>
2016-11-24 18:25:53 -0800

    Initial commit for question 31

---
+++ 31_StackPushPopOrder/StackPushPopOrder.cpp
@@ -0,0 +1,137 @@
+//==================================================================
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 著作权所有者：何海涛
+//==================================================================
+
+// 面试题30：包含min函数的栈
+// 题目：定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的min
+// 函数。在该栈中，调用min、push及pop的时间复杂度都是O(1)。
+
+#include "cstdio"
+#include <stack>
+
+bool IsPopOrder(const int* pPush, const int* pPop, int nLength)
+{
+    bool bPossible = false;
+
+    if(pPush != nullptr && pPop != nullptr && nLength > 0)
+    {
+        const int* pNextPush = pPush;
+        const int* pNextPop = pPop;
+
+        std::stack<int> stackData;
+
+        while(pNextPop - pPop < nLength)
+        {
+            // 当辅助栈的栈顶元素不是要弹出的元素
+            // 先压入一些数字入栈
+            while(stackData.empty() || stackData.top() != *pNextPop)
+            {
+                // 如果所有数字都压入辅助栈了，退出循环
+                if(pNextPush - pPush == nLength)
+                    break;
+
+                stackData.push(*pNextPush);
+
+                pNextPush ++;
+            }
+
+            if(stackData.top() != *pNextPop)
+                break;
+
+            stackData.pop();
+            pNextPop ++;
+        }
+
+        if(stackData.empty() && pNextPop - pPop == nLength)
+            bPossible = true;
+    }
+
+    return bPossible;
+}
+
+// ====================测试代码====================
+void Test(const char* testName, const int* pPush, const int* pPop, int nLength, bool expected)
+{
+    if(testName != nullptr)
+        printf("%s begins: ", testName);
+
+    if(IsPopOrder(pPush, pPop, nLength) == expected)
+        printf("Passed.\n");
+    else
+        printf("failed.\n");
+}
+
+void Test1()
+{
+    const int nLength = 5;
+    int push[nLength] = {1, 2, 3, 4, 5};
+    int pop[nLength] = {4, 5, 3, 2, 1};
+    
+    Test("Test1", push, pop, nLength, true);
+}
+
+void Test2()
+{
+    const int nLength = 5;
+    int push[nLength] = {1, 2, 3, 4, 5};
+    int pop[nLength] = {3, 5, 4, 2, 1};
+    
+    Test("Test2", push, pop, nLength, true);
+}
+
+void Test3()
+{
+    const int nLength = 5;
+    int push[nLength] = {1, 2, 3, 4, 5};
+    int pop[nLength] = {4, 3, 5, 1, 2};
+    
+    Test("Test3", push, pop, nLength, false);
+}
+
+void Test4()
+{
+    const int nLength = 5;
+    int push[nLength] = {1, 2, 3, 4, 5};
+    int pop[nLength] = {3, 5, 4, 1, 2};
+    
+    Test("Test4", push, pop, nLength, false);
+}
+
+// push和pop序列只有一个数字
+void Test5()
+{
+    const int nLength = 1;
+    int push[nLength] = {1};
+    int pop[nLength] = {2};
+
+    Test("Test5", push, pop, nLength, false);
+}
+
+void Test6()
+{
+    const int nLength = 1;
+    int push[nLength] = {1};
+    int pop[nLength] = {1};
+
+    Test("Test6", push, pop, nLength, true);
+}
+
+void Test7()
+{
+    Test("Test7", nullptr, nullptr, 0, false);
+}
+ 
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+    Test7();
+
+    return 0;
+}
+
