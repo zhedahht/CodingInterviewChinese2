fb1d29bd5fb54a8232b7ad9f9a055656930eb844
Harry He <zhedahht@hotmail.com>
2016-11-24 18:50:10 -0800

    Initial commit for question 32(3)

--- 32_03_PrintTreesInZigzag/PrintTreesInZigzag.cpp
+++ 32_03_PrintTreesInZigzag/PrintTreesInZigzag.cpp
@@ -3,46 +3,52 @@
 // 作者：何海涛
 //==================================================================
 
-// 面试题32（二）：分行从上到下打印二叉树
-// 题目：从上到下按层打印二叉树，同一层的结点按从左到右的顺序打印，每一层
-// 打印到一行。
+// 面试题32（三）：之字形打印二叉树
+// 题目：请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺
+// 序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，
+// 其他行以此类推。
 
 #include "cstdio"
 #include "..\Utilities\BinaryTree.h"
-#include <queue>
+#include <stack>
 
 void Print(BinaryTreeNode* pRoot)
 {
     if(pRoot == nullptr)
         return;
 
-    std::queue<BinaryTreeNode*> nodes;
-    nodes.push(pRoot);
-    int nextLevel = 0;
-    int toBePrinted = 1;
-    while(!nodes.empty())
+    std::stack<BinaryTreeNode*> levels[2];
+    int current = 0;
+    int next = 1;
+
+    levels[current].push(pRoot);
+    while(!levels[0].empty() || !levels[1].empty())
     {
-        BinaryTreeNode* pNode = nodes.front();
+        BinaryTreeNode* pNode = levels[current].top();
+        levels[current].pop();
+
         printf("%d ", pNode->m_nValue);
 
-        if(pNode->m_pLeft != nullptr)
+        if(current == 0)
         {
-            nodes.push(pNode->m_pLeft);
-            ++nextLevel;
+            if(pNode->m_pLeft != nullptr)
+                levels[next].push(pNode->m_pLeft);
+            if(pNode->m_pRight != nullptr)
+                levels[next].push(pNode->m_pRight);
         }
-        if(pNode->m_pRight != nullptr)
+        else
         {
-            nodes.push(pNode->m_pRight);
-            ++nextLevel;
+            if(pNode->m_pRight != nullptr)
+                levels[next].push(pNode->m_pRight);
+            if(pNode->m_pLeft != nullptr)
+                levels[next].push(pNode->m_pLeft);
         }
 
-        nodes.pop();
-        --toBePrinted;
-        if(toBePrinted == 0)
+        if(levels[current].empty())
         {
             printf("\n");
-            toBePrinted = nextLevel;
-            nextLevel = 0;
+            current = 1 - current;
+            next = 1 - next;
         }
     }
 }
@@ -68,7 +74,7 @@ void Test1()
     printf("====Test1 Begins: ====\n");
     printf("Expected Result is:\n");
     printf("8 \n");
-    printf("6 10 \n");
+    printf("10 6 \n");
     printf("5 7 9 11 \n\n");
 
     printf("Actual Result is: \n");
@@ -186,6 +192,50 @@ void Test6()
     printf("\n");
 }
 
+//                8
+//        4              12
+//     2     6       10      14
+//   1  3  5  7     9 11   13  15
+void Test7()
+{
+    BinaryTreeNode* pNode8 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNode12 = CreateBinaryTreeNode(12);
+    BinaryTreeNode* pNode2 = CreateBinaryTreeNode(2);
+    BinaryTreeNode* pNode6 = CreateBinaryTreeNode(6);
+    BinaryTreeNode* pNode10 = CreateBinaryTreeNode(10);
+    BinaryTreeNode* pNode14 = CreateBinaryTreeNode(14);
+    BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+    BinaryTreeNode* pNode3 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+    BinaryTreeNode* pNode7 = CreateBinaryTreeNode(7);
+    BinaryTreeNode* pNode9 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNode11 = CreateBinaryTreeNode(11);
+    BinaryTreeNode* pNode13 = CreateBinaryTreeNode(13);
+    BinaryTreeNode* pNode15 = CreateBinaryTreeNode(15);
+
+    ConnectTreeNodes(pNode8, pNode4, pNode12);
+    ConnectTreeNodes(pNode4, pNode2, pNode6);
+    ConnectTreeNodes(pNode12, pNode10, pNode14);
+    ConnectTreeNodes(pNode2, pNode1, pNode3);
+    ConnectTreeNodes(pNode6, pNode5, pNode7);
+    ConnectTreeNodes(pNode10, pNode9, pNode11);
+    ConnectTreeNodes(pNode14, pNode13, pNode15);
+
+    printf("====Test7 Begins: ====\n");
+    printf("Expected Result is:\n");
+    printf("8 \n");
+    printf("12 4 \n");
+    printf("2 6 10 14 \n");
+    printf("15 13 11 9 7 5 3 1 \n\n");
+
+    printf("Actual Result is: \n");
+    Print(pNode8);
+    printf("\n");
+
+    DestroyTree(pNode8);
+}
+
 int main(int argc, char* argv[])
 {
     Test1();
@@ -194,7 +244,7 @@ int main(int argc, char* argv[])
     Test4();
     Test5();
     Test6();
+    Test7();
 
     return 0;
 }
-
