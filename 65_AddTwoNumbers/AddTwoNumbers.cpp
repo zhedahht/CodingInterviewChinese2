5d126d71253b228818f528c70e8aaf2611a93646
Harry He <zhedahht@hotmail.com>
2016-11-25 23:34:14 -0800

    Initial commit for question 65, 66

---
+++ 65_AddTwoNumbers/AddTwoNumbers.cpp
@@ -0,0 +1,53 @@
+//==================================================================
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 作者：何海涛
+//==================================================================
+
+// 面试题65：不用加减乘除做加法
+// 题目：写一个函数，求两个整数之和，要求在函数体内不得使用＋、－、×、÷
+// 四则运算符号。
+
+#include <cstdio>
+
+int Add(int num1, int num2)
+{
+    int sum, carry;
+    do
+    {
+        sum = num1 ^ num2;
+        carry = (num1 & num2) << 1;
+
+        num1 = sum;
+        num2 = carry;
+    }
+    while(num2 != 0);
+
+    return num1;
+}
+
+// ====================测试代码====================
+void Test(int num1, int num2, int expected)
+{
+    int result = Add(num1, num2);
+    if(result == expected)
+        printf("%d + %d is %d. Passed\n", num1, num2, result);
+    else
+        printf("%d + %d is %d. FAILED\n", num1, num2, result);
+}
+
+int main(int argc, char* argv[])
+{
+    Test(1, 2, 3);
+    Test(111, 899, 1010);
+
+    Test(-1, 2, 1);
+    Test(1, -2, -1);
+
+    Test(3, 0, 3);
+    Test(0, -4, -4);
+
+    Test(-2, -8, -10);
+
+    return 0;
+}
+
---
+++ 66_ConstuctArray/ConstuctArray.cpp
@@ -0,0 +1,135 @@
+//==================================================================
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 作者：何海涛
+//==================================================================
+
+// 面试题66：构建乘积数组
+// 题目：给定一个数组A[0, 1, …, n-1]，请构建一个数组B[0, 1, …, n-1]，其
+// 中B中的元素B[i] =A[0]×A[1]×… ×A[i-1]×A[i+1]×…×A[n-1]。不能使用除法。
+
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+void BuildProductionArray(const vector<double>& input, vector<double>& output)
+{
+    int length1 = input.size();
+    int length2 = output.size();
+
+    if(length1 == length2 && length2 > 1)
+    {
+        output[0] = 1;
+        for(int i = 1; i < length1; ++i)
+        {
+            output[i] = output[i - 1] * input[i - 1];
+        }
+
+        double temp = 1;
+        for(int i = length1 - 2; i >= 0; --i)
+        {
+            temp *= input[i + 1];
+            output[i] *= temp;
+        }
+    }
+}
+
+//================= Test Code =================
+static bool EqualArrays(const vector<double>& input, const vector<double>& output)
+{
+    int length1 = input.size();
+    int length2 = output.size();
+
+    if(length1 != length2)
+        return false;
+
+    for(int i = 0; i < length1; ++i)
+    {
+        if(abs(input[i] - output[i]) > 0.0000001)
+            return false;
+    }
+
+    return true;
+}
+
+static void test(char* testName, const vector<double>& input, vector<double>& output, const vector<double>& expected)
+{
+    printf("%s Begins: ", testName);
+
+    BuildProductionArray(input, output);
+    if(EqualArrays(output, expected))
+        printf("Passed.\n");
+    else
+        printf("FAILED.\n");
+}
+
+static void test1()
+{
+    // 输入数组中没有0
+    double input[] = { 1, 2, 3, 4, 5 };
+    double output[] = { 0, 0, 0, 0, 0 };
+    double expected[] = { 120, 60, 40, 30, 24 };
+
+    test("Test1", vector<double>(input, input + sizeof(input) / sizeof(double)),
+        vector<double>(output, output + sizeof(output) / sizeof(double)),
+        vector<double>(expected, expected + sizeof(expected) / sizeof(double)));
+}
+
+static void test2()
+{
+    // 输入数组中有一个0
+    double input[] = { 1, 2, 0, 4, 5 };
+    double output[] = { 0, 0, 0, 0, 0 };
+    double expected[] = { 0, 0, 40, 0, 0 };
+
+    test("Test2", vector<double>(input, input + sizeof(input) / sizeof(double)),
+        vector<double>(output, output + sizeof(output) / sizeof(double)),
+        vector<double>(expected, expected + sizeof(expected) / sizeof(double)));
+}
+
+static void test3()
+{
+    // 输入数组中有两个0
+    double input[] = { 1, 2, 0, 4, 0 };
+    double output[] = { 0, 0, 0, 0, 0 };
+    double expected[] = { 0, 0, 0, 0, 0 };
+
+    test("Test3", vector<double>(input, input + sizeof(input) / sizeof(double)),
+        vector<double>(output, output + sizeof(output) / sizeof(double)),
+        vector<double>(expected, expected + sizeof(expected) / sizeof(double)));
+}
+
+static void test4()
+{
+    // 输入数组中有正、负数
+    double input[] = { 1, -2, 3, -4, 5 };
+    double output[] = { 0, 0, 0, 0, 0 };
+    double expected[] = { 120, -60, 40, -30, 24 };
+
+    test("Test4", vector<double>(input, input + sizeof(input) / sizeof(double)),
+        vector<double>(output, output + sizeof(output) / sizeof(double)),
+        vector<double>(expected, expected + sizeof(expected) / sizeof(double)));
+}
+
+static void test5()
+{
+    // 输入输入中只有两个数字
+    double input[] = { 1, -2 };
+    double output[] = { 0, 0 };
+    double expected[] = { -2, 1 };
+
+    test("Test5", vector<double>(input, input + sizeof(input) / sizeof(double)),
+        vector<double>(output, output + sizeof(output) / sizeof(double)),
+        vector<double>(expected, expected + sizeof(expected) / sizeof(double)));
+}
+
+int main(int argc, char* argv[])
+{
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
+
+    return 0;
+}
