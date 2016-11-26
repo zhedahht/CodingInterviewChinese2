61800d99552eae551aa4c67b35b4f62c683d0b75
Harry He <zhedahht@hotmail.com>
2016-11-25 21:31:05 -0800

    Initial commit for question 57(1)

---
+++ 57_01_TwoNumbersWithSum/TwoNumbersWithSum.cpp
@@ -0,0 +1,102 @@
+//==================================================================
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 作者：何海涛
+//==================================================================
+
+// 面试题57（一）：和为s的两个数字
+// 题目：输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们
+// 的和正好是s。如果有多对数字的和等于s，输出任意一对即可。
+
+#include <cstdio>
+
+bool FindNumbersWithSum(int data[], int length, int sum, 
+                        int* num1, int* num2)
+{
+    bool found = false;
+    if(length < 1 || num1 == nullptr || num2 == nullptr)
+        return found;
+
+    int ahead = length - 1;
+    int behind = 0;
+
+    while(ahead > behind)
+    {
+        long long curSum = data[ahead] + data[behind];
+
+        if(curSum == sum)
+        {
+            *num1 = data[behind];
+            *num2 = data[ahead];
+            found = true;
+            break;
+        }
+        else if(curSum > sum)
+            ahead --;
+        else
+            behind ++;
+    }
+
+    return found;
+}
+
+// ====================测试代码====================
+void Test(const char* testName, int data[], int length, int sum, bool expectedReturn)
+{
+    if(testName != nullptr)
+        printf("%s begins: ", testName);
+    
+    int num1, num2;
+    int result = FindNumbersWithSum(data, length, sum, &num1, &num2);
+    if(result == expectedReturn)
+    {
+        if(result)
+        {
+            if(num1 + num2 == sum)
+                printf("Passed. \n");
+            else
+                printf("FAILED. \n");
+        }
+        else
+            printf("Passed. \n");
+    }
+    else
+        printf("FAILED. \n");
+}
+
+// 存在和为s的两个数字，这两个数字位于数组的中间
+void Test1()
+{
+    int data[] = {1, 2, 4, 7, 11, 15};
+    Test("Test1", data, sizeof(data) / sizeof(int), 15, true);
+}
+
+// 存在和为s的两个数字，这两个数字位于数组的两段
+void Test2()
+{
+    int data[] = {1, 2, 4, 7, 11, 16};
+    Test("Test2", data, sizeof(data) / sizeof(int), 17, true);
+}
+
+// 不存在和为s的两个数字
+void Test3()
+{
+    int data[] = {1, 2, 4, 7, 11, 16};
+    Test("Test3", data, sizeof(data) / sizeof(int), 10, false);
+}
+
+// 鲁棒性测试
+void Test4()
+{
+    Test("Test4", nullptr, 0, 0, false);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+
+    return 0;
+}
+
