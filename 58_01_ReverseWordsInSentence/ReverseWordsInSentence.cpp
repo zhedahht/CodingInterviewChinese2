d0836e70fdc3126741af3faff81209c8e93db559
Harry He <zhedahht@hotmail.com>
2016-11-25 22:06:44 -0800

    Initial commit for question 58(1)

---
+++ 58_01_ReverseWordsInSentence/ReverseWordsInSentence.cpp
@@ -0,0 +1,114 @@
+//==================================================================
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 作者：何海涛
+//==================================================================
+
+// 面试题57（二）：为s的连续正数序列
+// 题目：输入一个正数s，打印出所有和为s的连续正数序列（至少含有两个数）。
+// 例如输入15，由于1+2+3+4+5=4+5+6=7+8=15，所以结果打印出3个连续序列1～5、
+// 4～6和7～8。
+
+#include <cstdio>
+#include "..\Utilities\StringUtil.h"
+#include <string>
+
+char* ReverseSentence(char *pData)
+{
+    if(pData == nullptr)
+        return nullptr;
+
+    char *pBegin = pData;
+
+    char *pEnd = pData;
+    while(*pEnd != '\0')
+        pEnd ++;
+    pEnd--;
+
+    // 翻转整个句子
+    Reverse(pBegin, pEnd);
+
+    // 翻转句子中的每个单词
+    pBegin = pEnd = pData;
+    while(*pBegin != '\0')
+    {
+        if(*pBegin == ' ')
+        {
+            pBegin ++;
+            pEnd ++;
+        }
+        else if(*pEnd == ' ' || *pEnd == '\0')
+        {
+            Reverse(pBegin, --pEnd);
+            pBegin = ++pEnd;
+        }
+        else
+            pEnd ++;
+    }
+
+    return pData;
+}
+
+// ====================测试代码====================
+void Test(const char* testName, char* input, const char* expectedResult)
+{
+    if(testName != nullptr)
+        printf("%s begins: ", testName);
+
+    ReverseSentence(input);
+
+    if((input == nullptr && expectedResult == nullptr)
+        || (input != nullptr && strcmp(input, expectedResult) == 0))
+        printf("Passed.\n\n");
+    else
+        printf("Failed.\n\n");
+}
+
+// 功能测试，句子中有多个单词
+void Test1()
+{
+    char input[] = "I am a student.";
+    char expected[] = "student. a am I";
+
+    Test("Test1", input, expected);
+}
+
+// 功能测试，句子中只有一个单词
+void Test2()
+{
+    char input[] = "Wonderful";
+    char expected[] = "Wonderful";
+
+    Test("Test2", input, expected);
+}
+
+// 鲁棒性测试
+void Test3()
+{
+    Test("Test3", nullptr, nullptr);
+}
+
+// 边界值测试，测试空字符串
+void Test4()
+{
+    Test("Test4", "", "");
+}
+
+// 边界值测试，字符串中只有空格
+void Test5()
+{
+    char input[] = "   ";
+    char expected[] = "   ";
+    Test("Test5", input, expected);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+
+    return 0;
+}
+
