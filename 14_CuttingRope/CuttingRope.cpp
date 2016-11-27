24ec58d06bb586497c1d91fce5f818226e60f0e1
Harry He <zhedahht@hotmail.com>
2016-11-26 22:20:07 -0800

    Changes for question 14

--- 14_CuttingRope/CuttingRope.cpp
+++ 14_CuttingRope/CuttingRope.cpp
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <cmath>
 
+// ====================动态规划====================
 int maxProductAfterCutting_solution1(int length)
 {
     if(length < 2)
@@ -47,6 +48,7 @@ int maxProductAfterCutting_solution1(int length)
     return max;
 }
 
+// ====================贪婪算法====================
 int maxProductAfterCutting_solution2(int length)
 {
     if(length < 2)
@@ -56,8 +58,12 @@ int maxProductAfterCutting_solution2(int length)
     if(length == 3)
         return 2;
 
+    // 尽可能多地减去长度为3的绳子段
     int timesOf3 = length / 3;
-    if((length - timesOf3 * 3) % 2 == 1)
+
+    // 当绳子最后剩下的长度为4的时候，不能再剪去长度为3的绳子段。
+    // 此时更好的方法是把绳子剪成长度为2的两段，因为2*2 > 3*1。
+    if(length - timesOf3 * 3 == 1)
         timesOf3 -= 1;
 
     int timesOf2 = (length - timesOf3 * 3) / 2;
@@ -171,4 +177,6 @@ int main(int agrc, char* argv[])
     test9();
     test10();
     test11();
+
+    return 0;
 }
