e28f8fc43df4e42a4f61bdce99c9b18a3d0699cd
Harry He <zhedahht@hotmail.com>
2016-11-27 14:57:05 -0800

    Changes on question 26, declaring the type of tree node value as double

--- 26_SubstructureInTree/SubstructureInTree.cpp
+++ 26_SubstructureInTree/SubstructureInTree.cpp
@@ -7,9 +7,16 @@
 // 题目：输入两棵二叉树A和B，判断B是不是A的子结构。
 
 #include <cstdio>
-#include "..\Utilities\BinaryTree.h"
+
+struct BinaryTreeNode
+{
+    double                 m_dbValue;
+    BinaryTreeNode*        m_pLeft;
+    BinaryTreeNode*        m_pRight;
+};
 
 bool DoesTree1HaveTree2(BinaryTreeNode* pRoot1, BinaryTreeNode* pRoot2);
+bool Equal(double num1, double num2);
 
 bool HasSubtree(BinaryTreeNode* pRoot1, BinaryTreeNode* pRoot2)
 {
@@ -17,7 +24,7 @@ bool HasSubtree(BinaryTreeNode* pRoot1, BinaryTreeNode* pRoot2)
 
     if(pRoot1 != nullptr && pRoot2 != nullptr)
     {
-        if(pRoot1->m_nValue == pRoot2->m_nValue)
+        if(Equal(pRoot1->m_dbValue, pRoot2->m_dbValue))
             result = DoesTree1HaveTree2(pRoot1, pRoot2);
         if(!result)
             result = HasSubtree(pRoot1->m_pLeft, pRoot2);
@@ -36,11 +43,54 @@ bool DoesTree1HaveTree2(BinaryTreeNode* pRoot1, BinaryTreeNode* pRoot2)
     if(pRoot1 == nullptr)
         return false;
 
-    if(pRoot1->m_nValue != pRoot2->m_nValue)
+    if(!Equal(pRoot1->m_dbValue, pRoot2->m_dbValue))
         return false;
 
     return DoesTree1HaveTree2(pRoot1->m_pLeft, pRoot2->m_pLeft) &&
         DoesTree1HaveTree2(pRoot1->m_pRight, pRoot2->m_pRight);
+}
+
+bool Equal(double num1, double num2)
+{
+    if((num1 - num2 > -0.0000001) && (num1 - num2 < 0.0000001))
+        return true;
+    else
+        return false;
+}
+
+// ====================辅助测试代码====================
+BinaryTreeNode* CreateBinaryTreeNode(double dbValue)
+{
+    BinaryTreeNode* pNode = new BinaryTreeNode();
+    pNode->m_dbValue = dbValue;
+    pNode->m_pLeft = nullptr;
+    pNode->m_pRight = nullptr;
+
+    return pNode;
+}
+
+void ConnectTreeNodes(BinaryTreeNode* pParent, BinaryTreeNode* pLeft, BinaryTreeNode* pRight)
+{
+    if(pParent != nullptr)
+    {
+        pParent->m_pLeft = pLeft;
+        pParent->m_pRight = pRight;
+    }
+}
+
+void DestroyTree(BinaryTreeNode* pRoot)
+{
+    if(pRoot != nullptr)
+    {
+        BinaryTreeNode* pLeft = pRoot->m_pLeft;
+        BinaryTreeNode* pRight = pRoot->m_pRight;
+
+        delete pRoot;
+        pRoot = nullptr;
+
+        DestroyTree(pLeft);
+        DestroyTree(pRight);
+    }
 }
 
 // ====================测试代码====================
