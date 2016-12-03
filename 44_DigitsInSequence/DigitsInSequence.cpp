678c5d3bcdc2b60c0be529de93468fc6da9654b5
Harry He <zhedahht@hotmail.com>
2016-12-02 22:25:48 -0800

    Minor changes for question 44

--- 44_DigitsInSequence/DigitsInSequence.cpp
+++ 44_DigitsInSequence/DigitsInSequence.cpp
@@ -9,6 +9,7 @@
 // 个函数求任意位对应的数字。
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -18,74 +19,69 @@ int beginNumber(int digits);
 
 int digitAtIndex(int index)
 {
-    if(index < 0)
-        return -1;
+	if(index < 0)
+		return -1;
 
-    int digits = 1;
-    while(true)
-    {
-        int numbers = countOfIntegers(digits);
-        if(index < numbers * digits)
-            return digitAtIndex(index, digits);
+	int digits = 1;
+	while(true)
+	{
+		int numbers = countOfIntegers(digits);
+		if(index < numbers * digits)
+			return digitAtIndex(index, digits);
 
-        index -= digits * numbers;
-        digits++;
-    }
+		index -= digits * numbers;
+		digits++;
+	}
 
-    return -1;
+	return -1;
 }
 
 int countOfIntegers(int digits)
 {
-    if(digits == 1)
-        return 10;
+	if(digits == 1)
+		return 10;
 
-    int count = 1;
-    for(int i = 1; i < digits; ++i)
-        count *= 10;
-    return 9 * count;
+	int count = (int) std::pow(10, digits - 1);
+	return 9 * count;
 }
 
 int digitAtIndex(int index, int digits)
 {
-    int number = beginNumber(digits) + index / digits;
-    int indexFromRight = digits - index % digits;
-    for(int i = 1; i < indexFromRight; ++i)
-        number /= 10;
-    return number % 10;
+	int number = beginNumber(digits) + index / digits;
+	int indexFromRight = digits - index % digits;
+	for(int i = 1; i < indexFromRight; ++i)
+		number /= 10;
+	return number % 10;
 }
 
 int beginNumber(int digits)
 {
-    if(digits == 1)
-        return 0;
+	if(digits == 1)
+		return 0;
 
-    int begin = 1;
-    for(int i = 1; i < digits; ++i)
-        begin *= 10;
-    return begin;
+	return (int) std::pow(10, digits - 1);
 }
 
 // ====================测试代码====================
 void test(const char* testName, int inputIndex, int expectedOutput)
 {
-    if(digitAtIndex(inputIndex) == expectedOutput)
-        cout << testName << " passed." << endl;
-    else
-        cout << testName << " FAILED." << endl;
+	if(digitAtIndex(inputIndex) == expectedOutput)
+		cout << testName << " passed." << endl;
+	else
+		cout << testName << " FAILED." << endl;
 }
 
 
 int main()
 {
-    test("Test1", 0, 0);
-    test("Test2", 1, 1);
-    test("Test3", 9, 9);
-    test("Test4", 10, 1);
-    test("Test5", 189, 9);  // 数字99的最后一位，9
-    test("Test6", 190, 1);  // 数字100的第一位，1
-    test("Test7", 1000, 3); // 数字370的第一位，3
-    test("Test8", 1001, 7); // 数字370的第二位，7
-    test("Test9", 1002, 0); // 数字370的第三位，0
-    return 0;
+	test("Test1", 0, 0);
+	test("Test2", 1, 1);
+	test("Test3", 9, 9);
+	test("Test4", 10, 1);
+	test("Test5", 189, 9);  // 数字99的最后一位，9
+	test("Test6", 190, 1);  // 数字100的第一位，1
+	test("Test7", 1000, 3); // 数字370的第一位，3
+	test("Test8", 1001, 7); // 数字370的第二位，7
+	test("Test9", 1002, 0); // 数字370的第三位，0
+	return 0;
 }
